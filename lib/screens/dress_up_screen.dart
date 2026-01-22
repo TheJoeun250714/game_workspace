69a1f578cc8e2bd701be57d500554af05a44539c
tje0714 <tje0714@outlook.kr>
2026-01-22 11:21:13 +0900

    dress_up

---
+++ lib/screens/dress_up_screen.dart
@@ -0,0 +1,29 @@
+
+import 'package:flutter/cupertino.dart';
+/*
+models
+└─ clothing_item.dart
+
+services
+└─ clothing_service.dart
+
+provider
+└─ dress_up_provider.dart
+
+widgets/dress_up
+└─ character_widget
+
+screen
+└─ dress_up_screen
+ */
+class DressUpScreen extends StatelessWidget{
+  const DressUpScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+
+
+}
