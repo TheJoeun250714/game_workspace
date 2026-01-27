52e82afeefeed99d7854f873a6c14cb082ba2344
tje0714 <tje0714@outlook.kr>
2026-01-27 12:19:52 +0900

    chat gemini

--- lib/widgets/chat/chat_bubble.dart
+++ lib/widgets/chat/chat_bubble.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 
 import '../../models/chat/chat_message.dart';
 
+// ChatBubble 자체를 ListView가 감싸고 있기 때문에 무조건 세로 정렬
+// 내부에있는 Row는 ListView  가 강제 세로 정렬을 하고 있기 때문에 의미 없음 XXXXXXX
+// Row 모두 지워도크게 문제되지 않는다.
 class ChatBubble extends StatelessWidget {
   final ChatMessage message;
 
@@ -19,6 +22,39 @@ class ChatBubble extends StatelessWidget {
     return Padding(
         padding: const EdgeInsets.all(8),
         child: Row(
+          // 이유 1 번 : 본인 메세지인지 상대 메세지 인지 구분하기 위해서
+          mainAxisAlignment:
+              message.isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            // 1. 말풍선
+            Flexible(
+                child: Container(
+              padding: const EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                  color: message.isUser ? Colors.blue : Colors.grey[300],
+                  borderRadius: BorderRadius.circular(12)),
+              child: Text(
+                message.message,
+                style: TextStyle(
+                    color: message.isUser ? Colors.white : Colors.black),
+              ),
+            )),
+            // 2. 시간 표시 (항상 말풍선 오른쪽에 위치)
+            const Padding(
+              padding: EdgeInsetsGeometry.only(left: 4, bottom: 2),
+              child: Text(
+                "10:20",
+                style: TextStyle(fontSize: 10, color: Colors.grey),
+              ),
+            )
+          ],
+        ));
+
+    /*
+    return Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          // 이유 1 번 : 본인 메세지인지 상대 메세지 인지 구분하기 위해서
           mainAxisAlignment: message.isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
           children: [
             Container(
@@ -45,5 +81,7 @@ class ChatBubble extends StatelessWidget {
           ],
         )
     );
+
+     */
   } // Widget build(BuildContext context) {
-}   // class ChatBubble extends StatelessWidget {
+} // class ChatBubble extends StatelessWidget {
